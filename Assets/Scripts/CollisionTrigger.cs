06f5882f5bc64cb2ef86e85b19f6dc29c8bb74e6
nburo <bureau.nic@gmail.com>
2013-11-29 20:42:37 -0500

    added collision detection
    debug collision detection + 2 new tagged sprites (one static, the other
    dynamic).

---
+++ Assets/Scripts/CollisionTrigger.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionTrigger : MonoBehaviour 
+{
+
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.gameObject.tag == "staticObjects") 
+		{
+			Debug.Log ("Enter static trigger");
+		}
+		else if (col.gameObject.tag == "dynamicObjects")
+		{
+			Debug.Log ("Enter dynamic trigger");
+		}
+	}
+
+	void OnTriggerExit(Collider col)
+	{
+		if (col.gameObject.tag == "staticObjects") 
+		{
+			Debug.Log ("Exit static trigger");
+		}
+		else if (col.gameObject.tag == "dynamicObjects")
+		{
+			Debug.Log ("Exit dynamic trigger");
+		}
+	}
+}
