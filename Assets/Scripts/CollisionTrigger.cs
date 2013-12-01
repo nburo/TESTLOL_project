73a0bc4aba6501a955a6a59ae5f38a421bbf0259
nburo <bureau.nic@gmail.com>
2013-12-01 11:45:31 -0500

    more collision corrections. more to come

--- Assets/Scripts/CollisionTrigger.cs
+++ Assets/Scripts/CollisionTrigger.cs
@@ -29,37 +29,52 @@ public class CollisionTrigger : MonoBehaviour
 
 
 
-		if (col.gameObject.tag == "staticObjects") { //if colliding static (non-movable objects)
+		if (col.gameObject.tag == "staticObjects" || col.gameObject.tag == "dynamicObjects") { //if colliding static (non-movable objects)
 
 
 			//for example, let's explain Collision_Right. The Right side of an object is a positive side (meaning it has a positive x coordinate
 			//relative to its center). In order for Collision_Right to be true :
 			//maxDiff.x most be >=0 (if the moving object gets inside the static object by jumping) so we know we are at least on the right side
 			//to know that we are indeed on the right (and nothing else), we must verify that |maxDiff.x| has indeed be the smallest value
-			Collision_Right = (maxDiff.x >= 0 && 
-					Mathf.Abs (maxDiff.x) <= Mathf.Abs (minDiff.x) && 
-					Mathf.Abs (maxDiff.x) <= Mathf.Abs (maxDiff.y) && 
-					Mathf.Abs (maxDiff.x) <= Mathf.Abs (minDiff.y));
-			Collision_Left = (minDiff.x <= 0 && 
-					Mathf.Abs (minDiff.x) <= Mathf.Abs (maxDiff.x) && 
-					Mathf.Abs (minDiff.x) <= Mathf.Abs (minDiff.y) &&
-					Mathf.Abs (minDiff.x) <= Mathf.Abs (maxDiff.y));
-			Collision_Up = (maxDiff.y >= 0 && 
-					Mathf.Abs (maxDiff.y) <= Mathf.Abs (minDiff.x) && 
-					Mathf.Abs (maxDiff.y) <= Mathf.Abs (maxDiff.x) && 
-					Mathf.Abs (maxDiff.y) <= Mathf.Abs (minDiff.y));
-			Collision_Down = (minDiff.y <= 0 && 
-					Mathf.Abs (minDiff.y) <= Mathf.Abs (minDiff.x) && 
-					Mathf.Abs (minDiff.y) <= Mathf.Abs (maxDiff.x) && 
-					Mathf.Abs (minDiff.y) <= Mathf.Abs (maxDiff.y));
-			Collision_Below = (maxDiff.z >= 0);
+			/*Collision_Right = (	Mathf.Abs (maxDiff.x) <= Mathf.Abs (maxDiff.y) && 
+								Mathf.Abs (maxDiff.x) <= Mathf.Abs (minDiff.y));
+			Collision_Left = (	Mathf.Abs (minDiff.x) <= Mathf.Abs (minDiff.y) &&
+								Mathf.Abs (minDiff.x) <= Mathf.Abs (maxDiff.y));
+			Collision_Up = (	Mathf.Abs (maxDiff.y) <= Mathf.Abs (minDiff.x) && 
+								Mathf.Abs (maxDiff.y) <= Mathf.Abs (maxDiff.x));
+			Collision_Down = (	Mathf.Abs (minDiff.y) <= Mathf.Abs (minDiff.x) && 
+								Mathf.Abs (minDiff.y) <= Mathf.Abs (maxDiff.x));
+			Collision_Below = (maxDiff.z >= 0);*/
+
+			if(Mathf.Abs (maxDiff.x) <= Mathf.Abs (maxDiff.y) && 
+			   Mathf.Abs (maxDiff.x) <= Mathf.Abs (minDiff.y) &&
+			   Mathf.Abs (maxDiff.x) <= Mathf.Abs (maxDiff.z) &&
+			   Mathf.Abs (maxDiff.x) <= Mathf.Abs (minDiff.z))
+				Collision_Right = true;
+			if(Mathf.Abs (minDiff.x) <= Mathf.Abs (minDiff.y) &&
+			   Mathf.Abs (minDiff.x) <= Mathf.Abs (maxDiff.y) &&
+			   Mathf.Abs (minDiff.x) <= Mathf.Abs (maxDiff.z) &&
+			   Mathf.Abs (minDiff.x) <= Mathf.Abs (minDiff.z))
+				Collision_Left = true;
+			if(Mathf.Abs (maxDiff.y) <= Mathf.Abs (minDiff.x) && 
+			   Mathf.Abs (maxDiff.y) <= Mathf.Abs (maxDiff.x) &&
+			   Mathf.Abs (maxDiff.y) <= Mathf.Abs (maxDiff.z) &&
+			   Mathf.Abs (maxDiff.y) <= Mathf.Abs (minDiff.z))
+				Collision_Up = true;
+			if(Mathf.Abs (minDiff.y) <= Mathf.Abs (minDiff.x) && 
+			   Mathf.Abs (minDiff.y) <= Mathf.Abs (maxDiff.x) &&
+			   Mathf.Abs (minDiff.y) <= Mathf.Abs (maxDiff.z) &&
+			   Mathf.Abs (minDiff.y) <= Mathf.Abs (minDiff.z))
+				Collision_Down = true;
+			if(maxDiff.z >= 0)
+				Collision_Below=true;
 
 			//below and over are not done yet
 
 			Debug.Log ("left:" + Collision_Left + " right:" + Collision_Right + " up:" + Collision_Up + " down:" + Collision_Down + minDiff.z);
 		
 		} 
-		else if (col.gameObject.tag == "dynamicObjects") 
+		else if (col.gameObject.tag == "otherObjects") 
 		{ //if colliding dynamic (movable) objects
 			Debug.Log ("Enter dynamic trigger");
 		} 
